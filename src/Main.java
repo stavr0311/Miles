a11be49aa90bedc8b612256d8c367c04f3f0db38
Stavr <rommman@inbox.ru>
2024-08-10 10:47:44 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 10_000;
+        int bonus = price / 20;
+        int miles = + bonus;
+        System.out.println("Сумма начисленных миль:"+miles);
+    }
 }
